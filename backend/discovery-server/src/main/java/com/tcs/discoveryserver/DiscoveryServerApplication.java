6cca93fe3df88a63d1b2eb22484a7543a433c5d7
ShankarKumarSuddamalla <suddamallashankarkumar@gmail.com>
2026-01-05 23:26:48 +0530

    Discovery Server Setup

--- backend/discovery-server/src/main/java/com/tcs/discoveryserver/DiscoveryServerApplication.java
+++ backend/discovery-server/src/main/java/com/tcs/discoveryserver/DiscoveryServerApplication.java
@@ -2,7 +2,10 @@ package com.tcs.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
+//@EnableEurekaServer turns this application into a service registry
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
